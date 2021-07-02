e3001dce0987762a723b63fd6f0d069530949805
Asheesh Laroia <asheesh@asheesh.org>
2021-07-01 19:52:19 -0700

    Click button, change label text

--- repro/src/repro/app.py
+++ repro/src/repro/app.py
@@ -16,7 +16,11 @@ class Repro(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
+        label = toga.Label("App started")
+        def change_label(*args):
+            label.text = "New text"
+        button = toga.Button(label="Click to change label", on_press=change_label)
+        main_box = toga.Box(children=[label, button])
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
